e2140d510a6eb84035ffb565c984f7c10202301a
valejmdev Müller <valentinjmueller055@gmail.com>
2024-05-16 16:26:14 +0000

    add eventlistener for storage and display of username

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    if (window.location.href !== 'index.html'){
+        const usernameSpan = document.getElementById('headline-username'); 
+        const storedUsername = sessionStorage.getItem('username');
+        usernameSpan.textContent = storedUsername;
+    }
+})
 
-
-
+console.log(window.location.href);
 const startForm = document.getElementById('username');
 const usernameInput = document.getElementById('username');
 
