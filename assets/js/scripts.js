4d9f9da743e6c66e0d0a26dff8399381c427da39
valejmdev Müller <valentinjmueller055@gmail.com>
2024-05-16 17:40:48 +0000

    fix storage issue of username and redirect

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,11 +6,12 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 })
 
-const startForm = document.getElementById('username');
+const startForm = document.getElementById('username-form');
 const usernameInput = document.getElementById('username');
 
-startForm.addEventListener('submit', (event) =>{
+document.getElementById('username-form').addEventListener('submit', function(event) {
     event.preventDefault();
     const username = usernameInput.value;
     sessionStorage.setItem('username', username);
+    startForm.submit();
 });
