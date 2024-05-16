fee9b0e22b101b0e05ffadcd94683dc6afe72429
valejmdev Müller <valentinjmueller055@gmail.com>
2024-05-16 19:04:41 +0000

    try fix for theme selector storage

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,7 @@
 //Page Load Function pasting username in H1
-document.addEventListener('DOMContentLoaded', function() {
-    if (document.title !== 'The nearly endless Text-Adventure Login'){
-        const usernameSpan = document.getElementById('headline-username'); 
+document.addEventListener('DOMContentLoaded', function () {
+    if (document.title !== 'The nearly endless Text-Adventure Login') {
+        const usernameSpan = document.getElementById('headline-username');
         const storedUsername = sessionStorage.getItem('username');
         usernameSpan.textContent = storedUsername;
     }
@@ -10,9 +10,17 @@ document.addEventListener('DOMContentLoaded', function() {
 const startForm = document.getElementById('username-form');
 const usernameInput = document.getElementById('username');
 
-document.getElementById('username-form').addEventListener('submit', function(event) {
+document.getElementById('username-form').addEventListener('submit', function (event) {
     event.preventDefault();
     const username = usernameInput.value;
     sessionStorage.setItem('username', username);
     startForm.submit();
+});
+//Theme Page 
+const themeForm = document.getElementById('theme-selector-form');
+
+document.getElementById('theme-selector-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    let theme = document.querySelector('input[name="theme-selection"]:checked').value;
+    sessionStorage.setItem('theme', theme);
 });
