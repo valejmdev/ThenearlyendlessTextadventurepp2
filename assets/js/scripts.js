7f24446035272ea71611be6f40a2c1f07a08c810
valejmdev Müller <valentinjmueller055@gmail.com>
2024-05-16 16:57:08 +0000

    change features of sitechange and structure

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,12 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-    if (window.location.href !== 'index.html'){
+    if (document.title !== 'The nearly endless Text-Adventure Login'){
         const usernameSpan = document.getElementById('headline-username'); 
         const storedUsername = sessionStorage.getItem('username');
         usernameSpan.textContent = storedUsername;
     }
 })
 
-console.log(window.location.href);
 const startForm = document.getElementById('username');
 const usernameInput = document.getElementById('username');
 
@@ -14,5 +13,4 @@ startForm.addEventListener('submit', (event) =>{
     event.preventDefault();
     const username = usernameInput.value;
     sessionStorage.setItem('username', username);
-    window.location.href = 'theme.html';
 });
